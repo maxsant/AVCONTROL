8f591179b7924906061b4d756e4a9c2cd1e0e100
maxsant <maxsant@gmail.com>
2024-03-10 16:43:54 -0500

    Cambiar modelo categoria a modelo usuario

---
+++ models/User.php
@@ -0,0 +1,105 @@
+<?php
+class User extends Connect
+{
+    /* TODO obtener usuario por ID */
+    public function getUserById($id)
+    {
+        $conectar = parent::connection();
+
+        $sql = '
+            SELECT 
+                *
+            FROM
+                users
+            WHERE
+                is_active = 1 AND id=?
+        ';
+
+        $query = $conectar->prepare($sql);
+        $query->bindValue(1,$id);
+        $query->execute();
+
+        return $query->fetch(PDO::FETCH_ASSOC);  
+    }
+    /* TODO eliminar usurio por ID */
+    public function deleteUserById($id)
+    {
+        $conectar = parent::connection();
+
+        $sql = '
+            UPDATE
+                users
+            SET
+                is_active = 0
+            WHERE
+                id=?
+        ';
+
+        $query = $conectar->prepare($sql);
+        $query->bindValue(1,$id);
+        $query->execute();
+
+        return $query->fetch(PDO::FETCH_ASSOC);   
+    }
+    /* TODO insertar usuario */
+    public function insertCategory($name, $lastname, $identification, $phone, $email, $password_hash, $role_id, $identification_type_id)
+    {
+        $conectar = parent::connection();
+
+        $sql = '
+            INSERT INTO
+                users (name, lastname, identification, phone, email, password_hash, role_id, identification_type_id, created)
+            VALUES
+                (?, ?, ?, ?, ?, ?, ?, ?, ?, now())
+        ';
+
+        $query = $conectar->prepare($sql);
+        $query->bindValue(1,$name);
+        $query->bindValue(2,$lastname);
+        $query->bindValue(3,$identification);
+        $query->bindValue(4,$phone);
+        $query->bindValue(5,$email);
+        $query->bindValue(6,$password_hash);
+        $query->bindValue(7,$role_id);
+        $query->bindValue(8,$identification_type_id);
+        $query->execute();
+
+        return $query->fetch(PDO::FETCH_ASSOC);   
+    }
+    /* TODO actualizar usuario por ID */
+    public function updateUserById($id, $name, $lastname, $identification, $phone, $email, $password_hash, $role_id, $identification_type_id)
+    {
+        $conectar = parent::connection();
+
+        $sql = '
+            UPDATE
+                users
+            SET
+                name = ?,
+                lastname = ?,
+                identification = ?,
+                phone = ?,
+                email = ?,
+                password_hash = ?,
+                role_id = ?,
+                identification_type_id = ?,
+            WHERE
+                id=?
+        ';
+
+        $query = $conectar->prepare($sql);
+        $query->bindValue(1,$name);
+        $query->bindValue(2,$lastname);
+        $query->bindValue(3,$identification);
+        $query->bindValue(4,$phone);
+        $query->bindValue(5,$email);
+        $query->bindValue(6,$password_hash);
+        $query->bindValue(7,$role_id);
+        $query->bindValue(8,$identification_type_id);
+        $query->bindValue(9,$id);
+        $query->execute();
+
+        return $query->fetch(PDO::FETCH_ASSOC);     
+    }
+}
+?>
