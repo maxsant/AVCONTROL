9d542f385b3f18680b8a86bb6e1c0fc49ae95ab3
maxsant <maxsant@gmail.com>
2024-03-15 13:05:06 -0500

    Code definition

--- api/v1/models/userModel.php
+++ api/v1/models/userModel.php
@@ -1,5 +1,5 @@
 <?php 
-require_once '../../../config/connection.php';
+require_once(__DIR__. '/../../../config/connection.php');
 
 class userModel extends Connect
 {
--- api/v1/routes/routes.php
+++ api/v1/routes/routes.php
@@ -1,3 +1,22 @@
 <?php 
+$routesArray=explode("/", $_SERVER['REQUEST_URI']);
 
+if (count(array_filter($routesArray)) ==3){
+    // Cuando no se hace una peticion
+    $json=[
+        "status" =>400,
+        "message" =>'Ruta incorrecta'
+    ];
+    echo json_encode($json,true);
+    return;
+}else{
+    if (count(array_filter($routesArray)) ==5 AND !is_numeric(array_filter($routesArray)[5])){
+        if (array_filter($routesArray)[4] == 'users'){
+            if (isset($_SERVER['REQUEST_METHOD']) AND $_SERVER['REQUEST_METHOD']=='GET'){
+                $users=new usersController();
+                $users->view();
+            }
+        }
+    }
+}
 ?>
